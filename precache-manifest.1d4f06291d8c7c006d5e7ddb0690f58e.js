227135d267ce2079a8907b23e0babf60a8cbaf16
nivrutti <nivrutti.khandade@gmail.com>
2020-09-06 17:08:24 -0400

    Updates

---
+++ precache-manifest.1d4f06291d8c7c006d5e7ddb0690f58e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b22298a1e9339571a02134455e92fa65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c60969f5ffb762b03f6",
+    "url": "/static/css/main.03c6ca28.chunk.css"
+  },
+  {
+    "revision": "3ca71b351f4a2fbf27b5",
+    "url": "/static/js/2.de1ca534.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.de1ca534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c60969f5ffb762b03f6",
+    "url": "/static/js/main.996994d8.chunk.js"
+  },
+  {
+    "revision": "8c0ed676e7fc0e04cf11",
+    "url": "/static/js/runtime-main.81dc9e71.js"
+  }
+]);
